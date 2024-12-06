a4cdb7612b8508ad4f1d608d08e4d2c8a351daae
JulianVerschoor <julian@hjmediagroep.nl>
2024-12-06 09:17:20 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFound from 'components/NotFound/NotFound';
 import Login from 'components/Login/Login';
 import Home from 'components/Home/Home';
 
@@ -9,7 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
-}
+} 
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Pagina niet gevonden</h1>
+      <p>De pagina die je zoekt bestaat niet.</p>
+    </div>
+  );
+}
